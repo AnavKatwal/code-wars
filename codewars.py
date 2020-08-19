eac208d09915a06122b4925ea3b297a1caee6315
Anav Katwal <anavkatwal@gmail.com>
2020-08-19 12:15:48 +0545

    Adding training code from codewars

---
+++ codewars.py
@@ -0,0 +1,16 @@
+# I was just trying to learn git hub.
+# So, sorry if you were looking for some thing.
+# But this piece of code is just a training code from code wars.
+
+def in_array(array1, array2):
+    in_list = []
+    for item in array1:
+        for i in array2:
+            if item in i and item not in in_list:
+                in_list.append(item)
+
+    return(in_list)
+
+
+print(in_array(["hi", "bye", "sar"], ["hibrew", "test", "bye bye", "sarcasm", "sars"]))
+
